09cdca0576d6667c5caf64d857f7a8ec7a58b98f
fathursyh <fathur.syah1@gmail.com>
2024-06-04 11:13:29 +0700

    minor update 1.0

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ import player
 
 app = Ursina()
 player.player
-
+bg_x = 24
+x = 13
 bg = Entity(
             model = 'quad', 
-            scale = (16, 9),
+            scale = (bg_x, 15),
+            position = (0, 0, 0),
             texture = 'assets/bg1.png',
             z = 1
 )
+duplicate(bg, x = bg_x)
 ground = Entity(
                 model = 'cube', 
-                y = -2,
+                y = -3.5,
                 scale_x = 10,
                 collider = 'box',
                 color = color.blue
 )
+duplicate(ground, x = x)
+
+camera.add_script(SmoothFollow(target=player.player, speed=4, offset=[2, 2, -40]))
+EditorCamera()
 app.run()
